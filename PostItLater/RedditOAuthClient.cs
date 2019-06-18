292bfe6f255ac79177d3a8ef02506a17f819b489
aHw <hello@lyxi.ca>
2019-06-18 12:21:44 -0700

    Fix missing Authorization when refreshing token

--- PostItLater/RedditOAuthClient.cs
+++ PostItLater/RedditOAuthClient.cs
@@ -92,6 +92,7 @@
         private void Refresh()
         {
             var client = CreateClient(TokenUrl, this.clientId);
+            client.Authenticator = new HttpBasicAuthenticator(this.clientId, string.Empty);
             var request = new RestRequest("api/v1/access_token", Method.POST);
             request.AddParameter("grant_type", "refresh_token");
             request.AddParameter("refresh_token", this.apikey.refresh);
